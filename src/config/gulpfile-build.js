33db85c5bfbc5dc83fad55b4e798f64b0004fa64
Wangaohang <ahianwang@163.com>
2019-10-10 22:34:02 +0800

    gulp发布模式搭建

---
+++ src/config/gulpfile-build.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const { src, dest, series, parallel } = require("gulp");
+const sass = require("gulp-sass");
+const webpack = require("webpack-stream");
+
+const cleanCSS = require('gulp-clean-css');
+const rev = require("gulp-rev");
+const revCollector =  require("gulp-rev-collector");
+
+const dirPath = "../../build/";
+function copyHTML() {
+    return src([`${dirPath}rev/**/*.json`,"../*.html"])
+        .pipe(revCollector())
+        .pipe(dest(`${dirPath}`))
+}
+function copySCSS() {
+    return src(["../styles/**/*.scss", "!../styles/yo/**/*.scss"])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rev())
+        .pipe(dest(`${dirPath}styles/`))
+        .pipe( rev.manifest() )
+        .pipe(dest(`${dirPath}rev/styles/`))
+}
+function packJS() {
+    return src(["../scripts/app.js"])
+        .pipe(webpack({
+            mode: "production",//production开发模式
+            entry: "../scripts/app.js",
+            output: {
+                path: path.resolve(__dirname, `${dirPath}`),//文件路径自动解析拼接
+                filename: 'app.js'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.html$/,
+                        loader: 'string-loader'
+                    }, {
+                        test: /\.art$/,
+                        loader: 'art-template-loader'
+                    }
+                ]
+            }
+        }))
+        .pipe(dest(`${dirPath}scripts/`))
+}
+function copyLibs() {
+    return src("../libs/**/*")
+        .pipe(dest(`${dirPath}libs/`))
+}
+function copyAssets() {
+    return src('../assets/**/*')
+        .pipe(dest(`${dirPath}/assets`))
+}
+exports.default =series(parallel(copySCSS, packJS, copyLibs, copyAssets) ,copyHTML);
